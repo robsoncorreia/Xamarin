5616305a1b58527f982496e63741efc4b1048f3c
Robson Correia <robsoncorreia01@outlook.com>
2019-01-06 09:41:00 -0200

    Adicionar arquivos de projeto.

---
+++ Xamarin/Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin.Master
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
